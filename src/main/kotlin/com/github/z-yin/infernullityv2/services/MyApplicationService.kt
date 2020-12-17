c782e3dc377b5fa59f0d8f6269d0bb283a8e455a
GitHub Action <action@github.com>
2020-12-17 02:10:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/z-yin/infernullityv2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/z-yin/infernullityv2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.z-yin.infernullityv2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.z-yin.infernullityv2.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/z-yin/infernullityv2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/z-yin/infernullityv2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.z-yin.infernullityv2.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.z-yin.infernullityv2.MyBundle
 
 class MyProjectService(project: Project) {
 
